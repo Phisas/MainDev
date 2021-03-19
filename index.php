a5e8c877996e970d6b41efe3a0a7c5502f722707
Teecloudy <teecloudy@qq.com>
2021-03-20 01:08:23 +0800

    Favcon and Logo
    Adding Favcon and logo

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Phisas | Public Health Information System Africa</title>
+        <meta name="description" content="Phisas | Public Health Information System Africa coming soon.">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="shortcut icon" href="includes/images/favicon.png" type="image/x-icon">
+    </head>
+</html>
